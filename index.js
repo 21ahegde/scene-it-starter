1746dd24aea73090be62e2e191ff25d61ab9f635
Aaron Hegde <hegde.aaron@gmail.com>
2020-03-07 13:01:51 -0500

    steps 1&2

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    function renderMovies (movieArray){
+       var movieHTML = movieArray.map(function (currentMovie){
+            return`
+            <div class="movie">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src=${currentMovie.Poster} alt="poster">
+                <div class="card-body">
+                  <h5 class="card-title">${currentMovie.Title}</h5>
+                  <p class="card-text">${currentMovie.Year}</p>
+                  <p class="card-text">${currentMovie.Type}</p>							  
+                  <a href="#" class="btn btn-primary">Add</a>
+                </div>
+              </div>
+        </div>        
+            `
+       }).join(""); 
+       return movieHTML; 
+    }
+    var moviesContainer = document.getElementById("movies-container");
+    document.getElementById("movies-container").addEventListener('search', function(e){
+        e.preventDefault();
+    });
+    /*
+    moviesContainer.innerHTML = renderMovies(movieData);
+    */
+});
+
+
